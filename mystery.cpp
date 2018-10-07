1f8acb9492a5ad8ae0704e6acbda958815e9f24e
ymirofjotunheim00 <ymirofjotunheim@users.noreply.github.com>
2018-10-06 23:20:22 -0400

    mystery sorts added

--- mystery.cpp
+++ mystery.cpp
@@ -25,6 +25,38 @@ void mystery1(auto& Data)
 
 }
 
+void mystery2(auto& Data)
+{
+	cout<<endl<<"Mystery 2"<<endl<<"----------------------------"<<endl;
+	for ( int i = 0; i < Data.size(); i++)
+	{
+		for ( int j = 0; j < Data.size() -1; j++)
+		{
+			if (Data[j+1] < Data[j])
+			{
+				swap(Data[j+1], Data[j]);
+			}		
+		}
+		print(Data);
+	}
+}
+
+void mystery3(auto& Data)
+{
+	cout<<endl<<"Mystery 3"<<endl<<"----------------------------"<<endl;
+	for ( int i = 0; i < Data.size(); i++)
+	{
+		for ( int j = i; j < Data.size(); j++)
+		{
+			if (Data[j] < Data[i])
+			{
+				swap(Data[i], Data[j]);
+			}		
+		}
+		print(Data);
+	}
+}
+
 //... Other mysteries...
 
 int main()
